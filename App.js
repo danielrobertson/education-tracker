922e889ce8a988330efa6b49043ff2e94799c417
Daniel Robertson <danielrobertson733@gmail.com>
2018-06-23 02:15:21 -0500

    hello world react native

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, SafeAreaView } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>Hello Alyssa!</Text>;
+      </SafeAreaView>
+    );
+  }
+}
